77032ed621d23810e35f3007563d583852d7995a
prabhat <prabhatiitbhu@gmail.com>
2017-10-05 19:12:27 +0530

    quiz

---
+++ Quiz/client/Quiz/index.android.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import { AppRegistry } from 'react-native';
+import AppContainer from './src/containers/AppContainer';
+
+AppRegistry.registerComponent('Quiz', () => AppContainer);
--- client/NativeStarter/index.android.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import { AppRegistry } from 'react-native';
-import AppContainer from './src/containers/AppContainer';
-
-AppRegistry.registerComponent('NativeStarter', () => AppContainer);
